c1bb4f38204d37dd0dd2967c1ee2122f8fff318b
Frederik Wylich <frederik.wylich@gmail.com>
2025-05-02 16:12:51 +0200

    all samples

--- sample_extractor.py
+++ sample_extractor.py
@@ -45,10 +45,10 @@ def create_train_val_splits(csv_path, n_samples=None, val_ratio=0.3, random_stat
 
 # Example usage:
 if __name__ == "__main__":
-    csv_path = "datasets_pytorch/ai4forest_camera/samples_eu.csv"
+    csv_path = "datasets_pytorch/ai4forest_camera/samples.csv"
     create_train_val_splits(
         csv_path=csv_path,
-        n_samples=1000,  # Set to None to use all samples
+        n_samples=11759,  # Set to None to use all samples
         val_ratio=0.1,    # 10% validation
         random_state=42   # For reproducibility
     )
